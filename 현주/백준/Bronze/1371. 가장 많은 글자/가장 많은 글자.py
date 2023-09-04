c92716a6cb5e9bac5b1e29e388959e0c36ba2394
hyunjoo <79887655+hhyunjooo@users.noreply.github.com>
2023-09-04 13:52:54 +0900

    [Bronze II] Title: 가장 많은 글자, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 현주/백준/Bronze/1371. 가장 많은 글자/가장 많은 글자.py
@@ -0,0 +1,13 @@
+import sys
+
+sentence = sys.stdin.read()
+alphabet = 'abcdefghijklmnopqrstuvwxyz'
+result = []
+
+for i in alphabet:
+    result.append(sentence.count(i))
+
+m = max(result)
+for i in range(len(result)):
+    if m == result[i]:
+        print(chr(i+97), end='')
