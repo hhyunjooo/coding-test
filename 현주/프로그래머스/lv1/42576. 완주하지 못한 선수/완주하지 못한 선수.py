7aa23f489f6b38104bb7b793e0c4d182a2870115
hyunjoo <79887655+hhyunjooo@users.noreply.github.com>
2023-09-12 09:37:07 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 75.66 ms, Memory: 39 MB -BaekjoonHub

---
+++ 현주/프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -0,0 +1,8 @@
+from collections import Counter
+
+def solution(participant, completion):
+    
+    result=Counter(participant)-Counter(completion)
+    answer=list(result.keys())[0]
+    return answer
+    
